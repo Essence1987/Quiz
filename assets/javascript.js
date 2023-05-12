80339e63e0682e55d08f3ef6a18290947306fb9f
Essence1987 <hwmelander@gmail.com>
2023-05-11 20:56:25 -0700

    added a intro and start button for quiz. Centered the button and the correct and incorrect messages

--- assets/javascript.js
+++ assets/javascript.js
@@ -30,24 +30,33 @@ let timeLeft = 30;
 
 // A function that displays the timer
 function showTimer() {
-  const timerElement = document.createElement("div");
-  timerElement.id = "timer";
-  timerElement.innerText = `Time left: ${timeLeft}s`;
-  document.querySelector("#quiz").appendChild(timerElement);
-
-  // Update the timer every second
-  const timerInterval = setInterval(() => {
-    timeLeft--;
+    const timerElement = document.createElement("div");
+    timerElement.id = "timer";
     timerElement.innerText = `Time left: ${timeLeft}s`;
-    if (timeLeft <= 0) {
-      clearInterval(timerInterval);
-      quizComplete();
+    document.querySelector("#quiz").appendChild(timerElement);
+  
+    // Start the timer only if it hasn't been started yet
+  if (currentQuestion === 0) {
+    const timerInterval = setInterval(() => {
+      timeLeft--;
+      timerElement.innerText = `Time left: ${timeLeft}s`;
+      if (timeLeft <= 0) {
+        clearInterval(timerInterval);
+        quizComplete();
+      }
+    }, 1000);
+  }
+    
+    // Update the score based on the remaining time when the quiz is complete
+    if (currentQuestion === questions.length) {
+      score += timeLeft;
     }
-  }, 1000);
-}
+  }
 
+  
 // A function that displays a question and its choices
 function showQuestion() {
+  document.getElementById('start-container').style.display = "none";
   const questionElement = document.createElement("div");
   questionElement.id = "question";
   questionElement.innerText = questions[currentQuestion].question;
@@ -86,38 +95,53 @@ function handleChoice(event) {
     const selectedChoice = event.target;
     const selectedAnswer = selectedChoice.dataset.answer;
     const correctAnswer = questions[currentQuestion].answer;
+    const resultContainer = document.createElement("div"); // Create a container element
+    resultContainer.style.textAlign = "center"; // Set the container element's text-align property to center
     const resultElement = document.createElement("div");
     resultElement.classList.add("result");
   
     // Check if the selected answer matches the correct answer
     if (selectedAnswer == correctAnswer) {
       const endTime = Date.now();
-    // Converts milliseconds to seconds
+      // Converts milliseconds to seconds
       const timeTaken = (endTime - questionStartTime) / 1000;
-    // Add to the user's score based on how quickly they answered the question correctly.
-    if (timeTaken <=30) {  
-    score += 30 - Math.floor(timeTaken);
-    }
+      // Add to the user's score based on how quickly they answered the question correctly.
+      if (timeTaken <= 30) {
+        score += 30 - Math.floor(timeTaken);
+      }
       resultElement.innerText = "Correct!";
       resultElement.classList.add("correct");
-      
+  
       // Move to the next question if there are more questions
       currentQuestion++;
       if (currentQuestion < questions.length) {
         showQuestion();
       } else {
+        // Update the score based on the remaining time when the quiz is complete
+        score += timeLeft;
         quizComplete();
       }
-      
-    } else {
-      resultElement.innerText = "Incorrect!";
-      resultElement.classList.add("incorrect");
-    }
   
-    // Add the result element to the results container
-    const resultsElement = document.querySelector("#results");
-    resultsElement.innerHTML = "";
-    resultsElement.appendChild(resultElement);
+      // Add the result element to the results container
+      const resultsElement = document.querySelector("#results");
+      resultsElement.innerHTML = "";
+      resultContainer.appendChild(resultElement); // Append the result element to the container element
+      resultsElement.appendChild(resultContainer); // Append the container element to the results container
+  
+      // Show the final score if the quiz is complete
+      if (currentQuestion === questions.length) {
+        quizComplete();
+      }
+    } else {
+      resultElement.innerText = "Incorrect! Please try again.";
+      resultElement.classList.add("incorrect");
+  
+      // Add the result element to the results container
+      const resultsElement = document.querySelector("#results");
+      resultsElement.innerHTML = "";
+      resultContainer.appendChild(resultElement); // Append the result element to the container element
+      resultsElement.appendChild(resultContainer); // Append the container element to the results container
+    }
   }
 
 // A function that displays a "quiz complete" message with the user's score
@@ -127,8 +151,3 @@ function quizComplete() {
   const resultsElement = document.querySelector("#results");
   resultsElement.innerHTML = "Quiz complete! Your score is " + score + ".";
 }
-
-// Display the first question when the page loads
-window.addEventListener('load', function() {
-  showQuestion();
-});
